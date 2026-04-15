e0b80458ccfce739ece0c6c716237160c15bee85
cafeteria <brozoski614@hotmail.com>
2026-04-15 20:30:32 -0300

    Actualizacion del trabajo de Logica

--- Logica_TP/Program.cs
+++ Logica_TP/Program.cs
@@ -11,25 +11,34 @@ namespace Array
             string[] nombre = new string[20];
             int[] edad = new int[20];
 
-            Console.WriteLine("Ingrese su Nombre: ");
-            nombre[0] = Console.ReadLine();
+            for (int i = 0; i < 20; i++) 
+            {
+                Console.WriteLine("Ingrese su Nombre: ");
+                nombre[0] = Console.ReadLine();
 
-            Console.WriteLine("Ingrese su Edad: ");
-            edad[0] = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese su Edad: ");
+                edad[0] = int.Parse(Console.ReadLine());
+            }
+
 
             Console.WriteLine("Validando datos del cliente....");
 
 
-            for (int i = 0; i < 1; i++)
+            for (int i = 0; i < 20; i++)
             {
-                if (edad[i] >= 18) {
+                Console.WriteLine("Cliente: " + (i + 1));
+
+                if (edad[i] >= 18)
+                {
                     Console.WriteLine(nombre[i] + " Puede ingrresar al Boliche.");
-                } else
+                }
+                else
                 {
                     Console.WriteLine(nombre[i] + " Vaya para la casa CAPO.");
                 }
             }
 
+            Console.WriteLine("Precione ENTER para finalizar....");
             Console.ReadLine();
         }
     }
